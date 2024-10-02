46a375d3b0c81278a4c8114b65b4d4eab72f0cc8
Amose <133123138+Amoseraja@users.noreply.github.com>
2024-10-02 22:32:14 +0530

    Add files via upload

---
+++ mycamu1/src/main/java/com/bookstore/Mycamu1Application.java
@@ -0,0 +1,13 @@
+package com.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mycamu1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mycamu1Application.class, args);
+	}
+
+}
